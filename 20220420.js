175af3f4e4a728cc0aa1dc3888d59f51486a5be6
Tim Tran <timtran47@gmail.com>
2022-04-20 17:34:13 -0700

    code war challenge 4-19-22

---
+++ 20220420.js
@@ -0,0 +1,17 @@
+// // You only need one - Beginner
+
+// You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+
+// Array can contain numbers or strings. X can be either.
+
+// Return true if the array contains the value, false if not.
+
+
+
+// solution
+
+function check(a, x) {
+    let answer = a.includes(x)
+    return answer
+  }
+
