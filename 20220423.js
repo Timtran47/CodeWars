bf67c375e897ba3342b6691b961289a19626e78b
Tim Tran <timtran47@gmail.com>
2022-04-23 11:45:49 -0700

    code war challenege 4-23-22

--- 20220423.js
+++ 20220423.js
@@ -1,6 +1,9 @@
-Calculate average
+// Calculate average
 
 
+// Write a function which calculates the average of the numbers in a given list.
+
+// Note: Empty arrays should return 0.
 
 //solution
 
