691d47c6b5fbb5fea79f0f87a459d558c31ad788
Tim Tran <timtran47@gmail.com>
2022-04-26 07:55:48 -0700

    code war challenge 4/26/22

--- 20220426.js
+++ 20220426.js
@@ -1,6 +1,10 @@
 //NAME SHUFFLER
 
+// Write a function that returns a string in which firstname is swapped with last name.
 
+// Example(Input --> Output)
+
+// "john McClane" --> "McClane john"
 
 
 //SOLUTION
