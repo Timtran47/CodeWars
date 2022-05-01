bc0cf1bbe5845d54c79114349266b0d260f35b92
Tim Tran <timtran47@gmail.com>
2022-05-01 15:55:23 -0700

    code war challenege 4/31/22

--- 20220431.js
+++ 20220431.js
@@ -1,4 +1,8 @@
-//REVERSED SEQUENCE
+// //REVERSED SEQUENCE
+
+// Build a function that returns an array of integers from n to 1 where n>0.
+
+// Example : n=5 --> [5,4,3,2,1]
 
 
 
@@ -12,5 +16,4 @@ const reverseSeq = n => {
     }return arr
   };
 
-  
   
