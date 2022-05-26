1d0670df85af15235051e4ecf197fd37b57b69c0
Tim Tran <timtran47@gmail.com>
2022-05-26 09:26:17 -0700

    codewar challenge 3-18-2022

---
+++ 20220318.js
@@ -0,0 +1,25 @@
+// Reversed sequence
+
+// Build a function that returns an array of integers from n to 1 where n>0.
+
+// Example : n=5 --> [5,4,3,2,1]
+
+// //Test cases
+// const chai = require("chai");
+// const assert = chai.assert;
+// chai.config.truncateThreshold=0;
+
+// describe("reverseSeq", function() {
+//   it("Sample Test", function() {
+//     assert.deepEqual(reverseSeq(5), [5, 4, 3, 2, 1]);
+//   });
+// });
+
+//solution
+function reverseSeq(n){
+    let newArr = []
+    for( i = n; i > 0 ; i--){
+      newArr.push(i)
+    }
+    return newArr
+  }
