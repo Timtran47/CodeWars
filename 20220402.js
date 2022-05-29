529b4adeb8f3182f7163b95d717027b33af43f2a
Tim Tran <timtran47@gmail.com>
2022-05-29 08:09:53 -0700

    code war challenege 4-2-22

---
+++ 20220402.js
@@ -0,0 +1,19 @@
+// //convert number to string
+
+// We need a function that can transform a number into a string.
+
+// What ways of achieving this do you know?
+
+// Examples:
+// 123 --> "123"
+// 999 --> "999"
+
+
+
+//solution
+function numberToString(num) {
+    // Return a string of the number here!
+    let text = num.toString();
+   return text;
+  }
+
