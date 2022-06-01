7ce49310d0436749b9fb3c435576e47d64689c15
Tim Tran <timtran47@gmail.com>
2022-06-01 06:47:08 -0700

    code war challenege 4-9-22

---
+++ 20220409.js
@@ -0,0 +1,18 @@
+// //Beginner - Lost Without a Map
+
+// Given an array of integers, return a new array with each value doubled.
+
+// For example:
+
+// [1, 2, 3] --> [2, 4, 6]
+
+
+
+
+
+// Solution
+
+function maps(x){
+    const newArr = x.map((num)=> num * 2)
+    return newArr;
+    }
