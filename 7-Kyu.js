2da329c8c8728ddd68fed57181e61023ee12f8e0
Tim Tran <timtran47@gmail.com>
2022-06-04 14:41:40 -0700

    code war challenege 6-20-22

--- 7-Kyu.js
+++ 7-Kyu.js
@@ -188,7 +188,12 @@ function getMiddle(s){
 
 //List Filtering 6/20/22
 
+// In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
 
+// Example
+// filter_list([1,2,'a','b']) == [1,2]
+// filter_list([1,'a','b',0,15]) == [1,0,15]
+// filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
 
 
 
@@ -206,10 +211,6 @@ function filter_list(l) {
 
 
 
-
-
-
-
-
+//completed
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
