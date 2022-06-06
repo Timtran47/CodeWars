dda62574acbd977c99a2af13c4286613594a091d
Tim Tran <timtran47@gmail.com>
2022-06-06 16:00:09 -0700

    code war challenege 4-29-22

---
+++ 20220429.js
@@ -0,0 +1,30 @@
+// FUNDAMENTALS: RETURN
+
+
+
+
+
+//SOLUTION:
+function add(a,b){
+    return a + b
+}
+
+function divide(a,b){
+    return a/b
+}
+
+function multiply(a,b){
+    return a*b
+}
+
+function mod(a,b){
+    return a%b
+}
+   
+function exponent(a,b){
+    return a**b
+}
+    
+function subt(a,b){
+    return a-b
+}
