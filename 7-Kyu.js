8186520355feea3a85158c45d8ac3bc980235179
Tim Tran <timtran47@gmail.com>
2022-07-13 11:46:31 -0700

    Code War Challenege 7/13/22

--- 7-Kyu.js
+++ 7-Kyu.js
@@ -433,3 +433,30 @@ function getSum( a,b )
 
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+///Friend or Foe? 7/13/22
+
+// Make a program that filters a list of strings and returns a list with only your friends name in it.
+
+// If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...
+
+// Ex: Input = ["Ryan", "Kieran", "Jason", "Yous"], Output = ["Ryan", "Yous"]
+
+// i.e.
+
+// friend ["Ryan", "Kieran", "Mark"] `shouldBe` ["Ryan", "Mark"]
+// Note: keep the original order of the names in the output.
+
+
+
+//solution
+
+const friend = friends => friends.filter(word => word.length == 4)
+
+
+//completed
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
