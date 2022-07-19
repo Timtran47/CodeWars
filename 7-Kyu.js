f5ebb8fde56f44f393083a5465ca161ddb4f8df0
Tim Tran <timtran47@gmail.com>
2022-07-19 15:36:03 -0700

    code war challenege 7/19/22

--- 7-Kyu.js
+++ 7-Kyu.js
@@ -541,3 +541,28 @@ function openOrSenior(data){
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+
+//Mumbling 7/19/2022
+
+// This time no story, no theory. The examples below show you how to write function accum:
+
+// Examples:
+// accum("abcd") -> "A-Bb-Ccc-Dddd"
+// accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+// accum("cwAt") -> "C-Ww-Aaa-Tttt"
+// The parameter of accum is a string which includes only letters from a..z and A..Z.
+
+
+
+//solution
+
+
+function accum(s) {
+  return s.split('').map((c, i) => (c.toUpperCase() + c.toLowerCase().repeat(i))).join('-');
+}
+
+
+//completed
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
