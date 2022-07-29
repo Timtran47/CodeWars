f92e283de753bfcb5e478a78d4f36b518c768617
Tim Tran <timtran47@gmail.com>
2022-07-29 11:47:55 -0700

    code war challenge 7-29

--- 7-Kyu.js
+++ 7-Kyu.js
@@ -758,3 +758,20 @@ function divCon(x){
   }
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//Reverse Words 7/29/22
+
+// Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+
+// Examples
+// "This is an example!" ==> "sihT si na !elpmaxe"
+// "double  spaces"      ==> "elbuod  secaps"
+
+
+//solution
+function reverseWords(str) {
+  return str.split(" ").reverse().join(" ").split("").reverse().join("")
+}
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
