5aa1877f0bac00329a240d383f7c096ad8a448c6
Tim Tran <timtran47@gmail.com>
2022-08-21 11:13:24 -0700

    code war challenge 8/21

--- 7-Kyu.js
+++ 7-Kyu.js
@@ -904,5 +904,36 @@ const sortByLength = array => array.sort( (a,b) => a.length - b.length)
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
+//Testing 1-2-3   8/21/22
+
+
+// Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.
+
+// Write a function which takes a list of strings and returns each line prepended by the correct number.
+
+// The numbering starts at 1. The format is n: string. Notice the colon and space in between.
+
+// Examples: (Input --> Output)
+
+// [] --> []
+// ["a", "b", "c"] --> ["1: a", "2: b", "3: c"]
+
+
+
+
+//solution
+var number=function(array){
+  if (array.length == 0 ){
+    return []
+  }else{
+    return array.map( function(item, idx){
+      return (idx + 1) + ": " + item
+    })
+  }
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
 
 
