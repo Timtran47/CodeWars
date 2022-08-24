69b59e33f5c5ad46b47f0a989e662ab2bf1a861f
Tim Tran <timtran47@gmail.com>
2022-08-24 15:10:59 -0700

    code war challenge 8/24

--- 7-Kyu.js
+++ 7-Kyu.js
@@ -959,4 +959,42 @@ function smallEnough(a, limit){
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
+//Multiply Word in String  8/24/22
+
+
+// You are to write an function that takes a string as it's first paramter. This string will be a string of words.
+
+// You are expected to then use the second parameter, which will be an integer, to find the corresponding word in the given string. The first word would be represented by 0.
+
+// Once you have the located string you are finally going to multiply by it the third provided paramater, which will also be an interger. You are additionally required to add a hyphen in between each word.
+
+// Example
+
+// modifyMultiply ("This is a string",3,5) 
+// Should return
+
+// "string-string-string-string-string"
+
+// Since the 3rd word is 'string'(starting from 0 remember) and the third paramater indicates that it should be repeated 5 times.
+
+// Simple. Good luck.
+
+
+//solution
+
+function modifyMultiply (str,loc,num) {
+  //split the string into an array to target the word at the location
+  let strArray = str.split(' ')
+  
+  // pull the word at the loc argument
+  let target = strArray[loc] + "-"
+  
+  //repeat the value of the array num times but remove the last "-"
+  return target.repeat(num).slice( 0, -1)
+  }
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
  
