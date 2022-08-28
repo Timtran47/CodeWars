c7c734ac48ae67cb35cf4be5ae3a1fcbe130dbe6
Tim Tran <timtran47@gmail.com>
2022-08-28 07:18:45 -0700

    code war challenge 8/27

--- 7-Kyu.js
+++ 7-Kyu.js
@@ -993,6 +993,32 @@ function modifyMultiply (str,loc,num) {
   return target.repeat(num).slice( 0, -1)
   }
 
+  
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//Find the Capitals 8/28/22
+
+
+// Instructions
+// Write a function that takes a single string (word) as argument. The function must return an ordered list containing the indexes of all capital letters in the string.
+
+// Example
+// Test.assertSimilar( capitals('CodEWaRs'), [0,3,4,6] );
+
+
+
+//solution
+const capitals = word => word.split('') // split word into an array
+
+  .map((letter, idx) => letter === letter.toUpperCase() ? idx : false)
+  // return a new array that gives the index of the capital letter
+
+  .filter(num => Number.isInteger(num));
+  // filter through each item in the new array and return 
+  // a newer array of just the index values
+
+
+
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
