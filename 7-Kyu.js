9bb971828e8dfa7fc65d04520610ddaa18e426c7
Tim Tran <timtran47@gmail.com>
2022-09-19 14:04:50 -0700

    code war challenge 9/19

--- 7-Kyu.js
+++ 7-Kyu.js
@@ -1070,4 +1070,41 @@ function stantonMeasure(arr){
 }
 
 
+  /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////'
+
+  //Find the middle element 9/19/22
+  
+  //   As a part of this Kata, you need to create a function that when provided with a triplet, returns the index of the numerical element that lies between the other two elements.
+
+  // The input to the function will be an array of three distinct numbers (Haskell: a tuple).
+
+  // For example:
+
+  // gimme([2, 3, 1]) => 0
+  // 2 is the number that fits between 1 and 3 and the index of 2 in the input array is 0.
+
+  // Another example (just to make sure it is clear):
+
+  // gimme([5, 10, 14]) => 1
+  // 10 is the number that fits between 5 and 14 and the index of 10 in the input array is 1.
+
+
+
+  //solution
+
+  function gimme (triplet) {
+  
+    //obtain middle value of the original array by sorting the three initial values
+    let middleValue = triplet.slice().sort((a,b) => a-b)[1]
+    
+    //obtain the position of the middle value in the original array
+    return triplet.indexOf(middleValue)
+      
+    }
+  
+
+
+
+
+
   /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
