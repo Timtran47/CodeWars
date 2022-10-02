eef75162a727ae8b475de3fec39ecfb78496ed71
Tim Tran <timtran47@gmail.com>
2022-10-02 09:12:21 -0700

    code war challenge 10/2

--- 7-Kyu.js
+++ 7-Kyu.js
@@ -1131,3 +1131,37 @@ function stantonMeasure(arr){
 
 
   /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  //Alternate Capilization 10/2/22
+
+
+  //   Given a string, capitalize the letters that occupy even indexes and odd indexes separately, and return as shown below. Index 0 will be considered even.
+
+  // For example, capitalize("abcdef") = ['AbCdEf', 'aBcDeF']. See test cases for more examples.
+
+  // The input will be a lowercase string with no spaces.
+
+  // Good luck!
+
+  // If you like this Kata, please try:
+
+
+
+  //solution
+
+  function capitalize(s){
+    let even = s.split('').map((c,i) => 
+          i % 2 == 0 ? c.toLowerCase() : c.toUpperCase()
+      ).join('')
+    
+    let odd = s.split('').map((c,i) => 
+          i % 2 == 1 ? c.toLowerCase() : c.toUpperCase()
+      ).join('')
+    
+    return [odd, even]
+  };
+
+
+
+
+  ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
