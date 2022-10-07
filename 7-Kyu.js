692bde866d7afac7bb3315cbe0791d66c84a6723
Tim Tran <timtran47@gmail.com>
2022-10-07 15:54:34 -0700

    code war challenge 10/7

--- 7-Kyu.js
+++ 7-Kyu.js
@@ -1187,3 +1187,28 @@ const removeDuplicateWords = s => [...new Set(s.split(' '))].join(' ')
 
 
   /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  //Bumps in the Road     10/7/2022
+
+  // Your car is old, it breaks easily. The shock absorbers are gone and you think it can handle about 15 more bumps before it dies totally.
+
+  // Unfortunately for you, your drive is very bumpy! Given a string showing either flat road (_) or bumps (n). If you are able to reach home safely by encountering 15 bumps or less, return Woohoo!, otherwise return Car Dead
+
+
+  // solution
+  
+  function bump(x){
+    let count = x.split('n').length
+    if(count > 16){
+      return "Car Dead"
+    }else{
+      return "Woohoo!"
+    }
+    
+   }
+
+
+
+
+
+  /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
