b8a801e3a9873207bede4ba1a8b0ef2e4a2ca174
Tim Tran <timtran47@gmail.com>
2022-10-27 13:30:03 -0700

    code war challenge 10/27

--- 6 Kyu.js
+++ 6 Kyu.js
@@ -72,3 +72,30 @@ const nthSmallest = (arr, pos) => arr.sort( (a,b) => a-b)[pos - 1]
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//Duplicate Encoder 10/27/22
+
+//The goal of this exercise is to convert a string to a new string where each character in the new string is "(" if that character appears only once in the original string, or ")" if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
+
+// Examples
+// "din"      =>  "((("
+// "recede"   =>  "()()()"
+// "Success"  =>  ")())())"
+// "(( @"     =>  "))((" 
+
+
+
+//Solution
+
+function duplicateEncode(word){
+  return word
+    .toLowerCase()
+    .split('')
+    .map( function (a, i, w) {
+      return w.indexOf(a) == w.lastIndexOf(a) ? '(' : ')'
+    })
+    .join('');
+  }
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
