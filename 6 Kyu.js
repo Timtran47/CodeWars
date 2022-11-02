6809ba9b5c35458c27a075e5406a10bce47d1eb4
Tim Tran <timtran47@gmail.com>
2022-11-02 15:01:23 -0700

    code war challenge 11/2

--- 6 Kyu.js
+++ 6 Kyu.js
@@ -124,3 +124,21 @@ function stringTransformer(str) {
 
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//Sort by Last Char 11/2/2022
+
+// Given a string of words (x), you need to return an array of the words, sorted alphabetically by the final character in each.
+
+// If two words have the same last letter, they returned array should show them in the order they appeared in the given string.
+
+// All inputs will be valid.
+
+
+//solution
+
+function last(x){
+  return x.split(" ").sort((a,b) => a.charCodeAt(a.length - 1) - b.charCodeAt(b.length-1))
+}
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
