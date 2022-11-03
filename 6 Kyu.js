04ab81731ba987ea995b02d628b7d201f27f367a
Tim Tran <timtran47@gmail.com>
2022-11-03 15:33:09 -0700

    code war challenge 11/3

--- 6 Kyu.js
+++ 6 Kyu.js
@@ -142,3 +142,35 @@ function last(x){
 
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//shorter concat [reverse longer] 11/3/2022
+
+// Given 2 strings, a and b, return a string of the form: shorter+reverse(longer)+shorter.
+
+// In other words, the shortest string has to be put as prefix and as suffix of the reverse of the longest.
+
+// Strings a and b may be empty, but not null (In C# strings may also be null. Treat them as if they are empty.).
+// If a and b have the same length treat a as the longer producing b+reverse(a)+b
+
+
+//My solution
+function shorter_reverse_longer(a,b){
+  if(a.length<b.length){
+    let short = a
+    let long = b.split('').reverse().join('')
+    return `${short}${long}${short}`
+  }else{
+    let short = b
+    let long = a.split('').reverse().join('')
+    return `${short}${long}${short}`
+  }
+}
+
+//optimal solution
+// function shorter_reverse_longer(a,b){
+//   return a.length >= b.length ? b + a.split('').reverse().join('') + b :
+//   a + b.split('').reverse().join('') + a;
+// }
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
