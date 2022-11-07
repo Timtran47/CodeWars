a50fb3fa093f4b1a7119b575dff4336149cc8a6e
Tim Tran <timtran47@gmail.com>
2022-11-07 15:20:59 -0800

    code war challenge 11/7

--- 6 Kyu.js
+++ 6 Kyu.js
@@ -174,3 +174,31 @@ function shorter_reverse_longer(a,b){
 
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+//11/7/2022
+// An ordered sequence of numbers from 1 to N is given. One number might have deleted from it, then the remaining numbers were mixed. Find the number that was deleted.
+
+// Example:
+
+// The starting array sequence is [1,2,3,4,5,6,7,8,9]
+// The mixed array with one deleted number is [3,2,4,6,7,8,1,9]
+// Your function should return the int 5.
+// If no number was deleted from the starting array, your function should return the int 0.
+
+// Note: N may be 1 or less (in the latter case, the first array will be []).
+
+
+
+//solution
+
+function findDeletedNumber(arr, mixArr) {
+  if (arr.length == mixArr.length){
+    return 0
+  }else{
+    return arr.reduce( (val, acc) => val + acc, 0) - mixArr.reduce( (val, acc) => val + acc, 0)
+  }
+}
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
