629fc3589119d616b6b23a950181eaed0f89fe86
Tim Tran <timtran47@gmail.com>
2022-11-21 14:35:45 -0800

    code war challenge 11/21

--- 7-Kyu.js
+++ 7-Kyu.js
@@ -1285,3 +1285,18 @@ function gimme (triplet) {
 
 
   ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  //Remove consecutive duplicate words 
+
+  // Your task is to remove all consecutive duplicate words from a string, leaving only first words entries. For example:
+
+  // "alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta"
+  
+  // --> "alpha beta gamma delta alpha beta gamma delta"
+
+  //solution
+  const removeConsecutiveDuplicates = s => s.split(' ').filter( (value, index, arr) => value !== arr[index + 1]).join(' ')
+
+
+
+  /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
