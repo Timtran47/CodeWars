c247afead541f03db52b7cada672ae9f57bbd09e
Tim Tran <timtran47@gmail.com>
2023-01-04 17:07:28 -0800

    code war challenge 1/4

--- 7-Kyu.js
+++ 7-Kyu.js
@@ -1300,3 +1300,33 @@ function gimme (triplet) {
 
 
   /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  //Simple Fun #176: Reverse Letter
+
+
+//   Task
+// Given a string str, reverse it and omit all non-alphabetic characters.
+
+// Example
+// For str = "krishan", the output should be "nahsirk".
+
+// For str = "ultr53o?n", the output should be "nortlu".
+
+// Input/Output
+// [input] string str
+// A string consists of lowercase latin letters, digits and symbols.
+
+// [output] a string
+
+
+
+//solution
+function reverseLetter(str) {
+ 
+  //remove all non-letters
+  const cleanStr= str.replace(/[^a-z]/gi,'').split('').reverse().join('')
+  return cleanStr
+}
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
