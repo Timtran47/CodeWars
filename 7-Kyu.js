d990f31605f899819f530735e6d442cfb8d32117
Tim Tran <timtran47@gmail.com>
2023-01-18 16:29:03 -0800

    code war challenge 1/18

--- 7-Kyu.js
+++ 7-Kyu.js
@@ -1347,3 +1347,27 @@ function sumTwoSmallestNumbers(numbers) {
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// Recursion #1 - Factorial 1/18/23
+
+// n mathematics, the factorial of a non-negative integer n, denoted by n!, is the product of all positive integers less than or equal to n. For example,
+
+// 5! = 5 * 4 * 3 * 2 * 1 = 120.
+
+// The value of 0! is 1.
+
+// #Your task
+
+// You have to create the function factorial that receives n and returns n!. You have to use recursion.
+
+
+//solution
+const factorial = n => {
+  if (n <= 1) return 1;
+  
+  return n * factorial(n - 1);
+};
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
