fc42f6aa7d76bc31c82fc37e78f7492eadc25114
Tim Tran <timtran47@gmail.com>
2023-02-27 16:03:09 -0800

    code war challenge 2/27/23

--- 6 Kyu.js
+++ 6 Kyu.js
@@ -365,3 +365,27 @@ function solution(number){
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//Is a number prime? 2/27/2022
+
+// Define a function that takes an integer argument and returns a logical value true or false depending on if the integer is a prime.
+
+// Per Wikipedia, a prime number ( or a prime ) is a natural number greater than 1 that has no positive divisors other than 1 and itself.
+
+//solution 
+
+function isPrime(num) {
+  if (num < 2) return false;
+  const limit = Math.sqrt(num);
+  for (let i = 2; i <= limit; ++i) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
