b438053ea3da26ff62304ea7db3a703806edbf23
Tim Tran <timtran47@gmail.com>
2023-03-29 14:35:05 -0700

    code war challenge 3/29/23

--- 7-Kyu.js
+++ 7-Kyu.js
@@ -1578,6 +1578,38 @@ const describeAge=age=>age<=12?"You're a(n) kid":age>12&&age<18?"You're a(n) tee
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+//Odd-Even String Sort 3.29
+
+// Given a string s. You have to return another string such that even-indexed and odd-indexed characters of s are grouped and groups are space-separated (see sample below)
+
+// Note: 
+// 0 is considered to be an even index. 
+// All input strings are valid with no spaces
+// input: 'CodeWars'
+// output 'CdWr oeas'
+
+// S[0] = 'C'
+// S[1] = 'o'
+// S[2] = 'd'
+// S[3] = 'e'
+// S[4] = 'W'
+// S[5] = 'a'
+// S[6] = 'r'
+// S[7] = 's'
+// Even indices 0, 2, 4, 6, so we have 'CdWr' as the first group
+// odd ones are 1, 3, 5, 7, so the second group is 'oeas'
+
+
+//solution
+
+
+
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 
 
 
